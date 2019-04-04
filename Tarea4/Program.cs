2d425ddcaf8bfbf2d86d6694792a157e93348d6e
videoboyone <40149800+videoboyone@users.noreply.github.com>
2019-04-03 20:37:45 -0700

    Tarea 3 mod

--- Tarea4/Program.cs
+++ Tarea4/Program.cs
@@ -2,6 +2,44 @@
 
 namespace Tarea4
 {
+    class A
+
+    {
+
+    public int a;
+
+    public A()
+
+        {
+
+        a = 10;
+
+        }
+
+    public _______ string Display()
+
+      {
+
+      return a.ToString();
+
+      }
+
+    }
+
+class B:A
+
+   {
+
+   public int b;
+
+   public B():base()
+
+   {
+
+    b = 15;
+
+   }
+   }
     class Program
     {
         static void Main(string[] args)
